74cd6b0c2dfeed5b41088c379f8c15c1b48d83e3
Shubham Jaydeokar <Shubham.Jaydeokar@ril.com>
2021-01-18 22:37:03 +0530

    Initial commit

---
+++ app/src/main/java/com/happy/itemlocationfinder/MainActivity.kt
@@ -0,0 +1,124 @@
+package com.happy.itemlocationfinder
+
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.LocationManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
+import java.util.*
+
+private lateinit var fusedLocationClient: FusedLocationProviderClient
+private lateinit var locationRequest: LocationRequest
+private lateinit var locationCallback: LocationCallback
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fetchLocation()
+    }
+
+    fun fetchLocation(){
+        val manager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            showAlertLocation()
+        }
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        getLocationUpdates()
+    }
+
+    fun showAlertLocation(){
+        val dialog = AlertDialog.Builder(this)
+        dialog.setMessage("Your location settings is set to Off, Please enable location to use this application")
+        dialog.setPositiveButton("Settings") { _, _ ->
+            val myIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(myIntent)
+        }
+        dialog.setNegativeButton("Cancel") { _, _ ->
+            finish()
+        }
+        dialog.setCancelable(false)
+        dialog.show()
+    }
+
+    private fun getLocationUpdates() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        locationRequest = LocationRequest()
+        locationRequest.interval = 50000
+        locationRequest.fastestInterval = 50000
+        locationRequest.smallestDisplacement = 170f //170 m = 0.1 mile
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY //according to your app
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                if (locationResult.locations.isNotEmpty()) {
+                    /*val location = locationResult.lastLocation
+                    Log.e("location", location.toString())*/
+                    val addresses: List<Address>?
+                    val geoCoder = Geocoder(applicationContext, Locale.getDefault())
+                    addresses = geoCoder.getFromLocation(
+                            locationResult.lastLocation.latitude,
+                            locationResult.lastLocation.longitude,
+                            1
+                    )
+                    if (addresses != null && addresses.isNotEmpty()) {
+                        val address: String = addresses[0].getAddressLine(0)
+                        val city: String = addresses[0].locality
+                        val state: String = addresses[0].adminArea
+                        val country: String = addresses[0].countryName
+                        val postalCode: String = addresses[0].postalCode
+                        val knownName: String = addresses[0].featureName
+                        Log.e("MyLocation", "$address $city $state $postalCode $country $knownName")
+                    }
+                }
+            }
+        }
+    }
+
+    // Start location updates
+    private fun startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            fusedLocationClient.requestLocationUpdates(
+                    locationRequest,
+                    locationCallback,
+                    null /* Looper */
+            )
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+
+    }
+
+    // Stop location updates
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    // Stop receiving location update when activity not visible/foreground
+    override fun onPause() {
+        super.onPause()
+        stopLocationUpdates()
+    }
+
+    // Start receiving location update when activity  visible/foreground
+    override fun onResume() {
+        super.onResume()
+        startLocationUpdates()
+    }
+}
